eaa871494fb998e06d2264fa5aa13994fe794625
AdhamMohamedSaleh <adham.work24@gmail.com>
2025-09-29 10:45:32 +0300

    screen reader

--- app/page.js
+++ app/page.js
@@ -9,8 +9,8 @@ import Image from "next/image";
 function Page() {
   return (
     <div >
-      <div aria-hidden="true" className=" -z-10">
-      <Image src="/bg-tablet-pattern.svg" alt="" width={1440} height={1440} priority className="fixed left-1/2 -top-20 lg:-top-10 lg:-translate-y-1/2 lg:-translate-x-1/3 -translate-y-1/10 -translate-x-1/4"/>
+      <div aria-hidden="true" className="">
+      <Image src="/bg-tablet-pattern.svg" alt="" width={1440} height={1440} priority className="fixed left-1/2 -top-20 -z-10 lg:-top-10 lg:-translate-y-1/2 lg:-translate-x-1/3 -translate-y-1/10 -translate-x-1/4"/>
       </div>
       <Navbar />
       <main>
